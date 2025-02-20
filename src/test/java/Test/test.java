fe80b9ccf45cf49af5be178d76e6ef3e9ceee207
kumari-rashmi11 <rashmi_k@pursuitsoftware.com>
2025-02-20 15:29:18 +0530

    Update test.java

--- src/test/java/Test/test.java
+++ src/test/java/Test/test.java
@@ -1,39 +1,44 @@
 package Test;
-
+ 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
+import org.testng.annotations.Listeners;
+ 
 import Helper.BaseTest;
 import Helper.Interactions;
+import Helper.TestCaseListener;  // Make sure this listener is implemented in the Helper package
 import testprojectt.testprojectt.App;
-
+ 
+@Listeners(TestCaseListener.class)
 public class test extends BaseTest {
-    
-   
+ 
     @Test(priority = 1)
     public void openGooglePage() {
+        System.out.println("Running test: openGooglePage");
         driver.get("https://www.google.com/");
         System.out.println("Page title is: " + driver.getTitle());
     }
-    
+ 
     @Test(priority = 2, retryAnalyzer = Helper.RetryAnalyzer.class)
-	public void Search_feature() {
-    	openGooglePage();	
-    	App aut = new App(driver);
-		Interactions interact = new Interactions(driver);
-		interact.executeWithDelay(() -> aut.searchValidation());
-	}
-
+    public void Search_feature() {
+        System.out.println("Running test: Search_feature");
+        openGooglePage();  
+        App aut = new App(driver);
+        Interactions interact = new Interactions(driver);
+        interact.executeWithDelay(() -> aut.searchValidation());
+    }
     @Test(priority = 3, retryAnalyzer = Helper.RetryAnalyzer.class)
-	public void Validate_second_JOIN_US_button_click_TC_003() {
-    	openGooglePage();	
-    	App aut = new App(driver);
-		Interactions interact = new Interactions(driver);
-		interact.executeWithDelay(() -> aut.clickFeelingluckyBtn());
-	}
-	 @AfterMethod
-		public void afterMethod() throws InterruptedException {
-			Thread.sleep(2000);
-			driver.quit();
-	 }    
+    public void Validate_second_JOIN_US_button_click_TC_003() {
+        System.out.println("Running test: Validate_second_JOIN_US_button_click_TC_003");
+        openGooglePage();  
+        App aut = new App(driver);
+        Interactions interact = new Interactions(driver);
+        interact.executeWithDelay(() -> aut.clickFeelingluckyBtn());
+    }
+    @AfterMethod
+    public void afterMethod() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }    
 }
+
