11ffe1e92b9f8ff27e9b430499685f02a89ad2ad
kumari-rashmi11 <rashmi_k@pursuitsoftware.com>
2025-02-27 16:19:26 +0530

    Update TestCaseListener.java

--- src/main/java/Helper/TestCaseListener.java
+++ src/main/java/Helper/TestCaseListener.java
@@ -16,8 +16,8 @@ public class TestCaseListener implements ITestListener {
     @Override public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
     @Override public void onStart(ITestContext context) {}
     @Override public void onFinish(ITestContext context) {}
-<<<<<<< HEAD
+
 }
-=======
+
 }
->>>>>>> 954a21f8d08575b4b44f3d4783b34ab593e193e1
+
